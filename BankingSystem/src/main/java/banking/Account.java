815a2b5ebd07de6bdfba306ee7f8f5e5fad6ae53
Shuvam Baksi <sbaksi51@gmail.com>
2021-10-14 21:46:18 +0530

    practice code for banking system

---
+++ BankingSystem/src/main/java/banking/Account.java
@@ -0,0 +1,55 @@
+package banking;
+
+/**
+ * Abstract bank account class.<br>
+ * <br>
+ *
+ * Private Variables:<br>
+ * {@link #accountHolder}: AccountHolder<br>
+ * {@link #accountNumber}: Long<br>
+ * {@link #pin}: int<br>
+ * {@link #balance}: double
+ */
+public abstract class Account {
+	private AccountHolder accountHolder;
+	private Long accountNumber;
+	private int pin;
+	private double balance;
+
+	protected Account(AccountHolder accountHolder, Long accountNumber, int pin, double startingDeposit) {
+		// complete the constructor
+		this.accountHolder = accountHolder;
+		this.accountNumber= accountNumber;
+		this.pin=pin;
+		balance = startingDeposit;
+	}
+
+	public AccountHolder getAccountHolder() {
+
+		return accountHolder;
+	}
+
+	public boolean validatePin(int attemptedPin) {
+		// complete the function
+        return true;
+	}
+
+	public double getBalance() {
+		// complete the function
+        return -1;
+	}
+
+	public Long getAccountNumber() {
+		// complete the function
+        return -1L;
+	}
+
+	public void creditAccount(double amount) {
+		// complete the function
+	}
+
+	public boolean debitAccount(double amount) {
+		// complete the function
+        return true;
+	}
+}
---
+++ BankingSystem/src/main/java/banking/Person.java
@@ -0,0 +1,21 @@
+package banking;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private int idNumber;
+
+	public Person(String firstName, String lastName, int idNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = idNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+}
